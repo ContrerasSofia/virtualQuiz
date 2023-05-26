8853d91f79e5f763ea1c78b814c6b3cefe4a2203
Sofia Contreras <cgsofiaelizabeth15@gmail.com>
2023-05-26 10:54:20 -0400

    timer, change of question and feedback

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,10 @@
 // variables to reference DOM elements
 var startBtn = document.getElementById('start');
 var timerEl = document.getElementById('time');
+var choicesEl = document.getElementById('choices');
 var currentQuestionIndex = 0;
 var time = 0;
+
 
 function startQuiz(event) {
  
@@ -13,22 +15,22 @@ function startQuiz(event) {
         
       time--;
 
-      if(time === 0 || currentQuestionIndex == (questions.length -1)) {
-        // Stops execution of action at set interval
+      if(time <= 0){
         clearInterval(timerInterval);
-        time = 0;
+        if(!(questions.length === currentQuestionIndex)) {
+        // Stops execution of action at set interval
         quizEnd();
-
+        }
+        time = 0;
       }
+      
+    timerEl.textContent = time;
 
-      timerEl.textContent = time;
-    
     }, 1000);
 }
 
 function quizBegin() {
     // hide start screen
-    console.log("entro a quizbegin");
     var startScreenEl = document.getElementById('start-screen');
     startScreenEl.setAttribute('class', 'hide');
   
@@ -59,26 +61,60 @@ function quizEnd() {
 }
 
 function getQuestion() {
-  var currentQuestion = questions[currentQuestionIndex];
+  if(questions.length === currentQuestionIndex)
+    quizEnd();
+  else{
+    var currentQuestion = questions[currentQuestionIndex];
 
-  // update title with current question
-  var titleEl = document.getElementById('question-title');
-  titleEl.textContent = currentQuestion.title; 
-  var list = document.createElement('ol');
-  document.getElementById('choices').appendChild(list);
+      // update title with current question
+      var titleEl = document.getElementById('question-title');
+      titleEl.textContent = currentQuestion.title; 
 
-  // loop over choices
-  for (var i = 0; i < currentQuestion.choices.length; i++) {
-    // create new button for each choice
-    var choiceNode = document.createElement('li');
-    choiceNode.textContent = currentQuestion.choices[i];
+      // loop over choices
+      for (var i = 0; i < currentQuestion.choices.length; i++) {
+      
+        // create new button for each choice
+        var choiceNode = document.createElement('button');
+        choiceNode.setAttribute('class', 'choice');
+        choiceNode.setAttribute('value', 'choice');
+        choiceNode.textContent = currentQuestion.choices[i];
 
-    // display on the page
-    list.appendChild(choiceNode);
+        // display on the page
+        document.getElementById('choices').appendChild(choiceNode);
+      }
   }
   
+}
+
+function clearQuestions(){
+  let element = document.getElementById("choices");
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+function questionClick(event) {
+  buttonEl = event.target;
+  var currentQuestion = questions[currentQuestionIndex]; 
+
+  var feedback = document.createElement('p');
+  feedback.setAttribute('class', 'feedback');
+
+  if(currentQuestion.answer == buttonEl.textContent){
+    feedback.textContent = "Correct!";
+  }else{
+    feedback.textContent = "Incorrect!";
+    time = time - 10;
+  }
+  
+  clearQuestions()
+  currentQuestionIndex++;
+  getQuestion();
+  document.getElementById('choices').appendChild(feedback);
 }
 
 // user clicks button to start quizz
 startBtn.addEventListener("click", startQuiz);
 
+// user selects an option
+choicesEl.addEventListener("click", questionClick);
