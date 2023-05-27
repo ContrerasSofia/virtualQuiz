cc762b6cb997cdc8bdd6eafd0adb19d4363a881c
Sofia Contreras <cgsofiaelizabeth15@gmail.com>
2023-05-26 22:09:26 -0400

    readme, reset.css, questions, and final sttings

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,32 +1,32 @@
 // list of all questions, choices, and answers
 var questions = [
   {
-    title: 'Test Question 1',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: 'Inside which HTML element do we put the JavaScript?',
+    choices: ['<javascript>', '<scripting>', '<js>', '<script>'],
+    answer: '<script>',
   },{
-    title: 'Test Question 2',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: 'JavaScript is a ___ -side programming language.',
+    choices: ['Client', 'Server', 'Both', 'None'],
+    answer: 'Both',
   },{
-    title: 'Test Question 3',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: 'Can you guess the result? console.log(("b" + "a" + + "a" + "a").toLowerCase());',
+    choices: ['banana', 'BAAA', 'baaa', 'babaa'],
+    answer: 'baaa',
   },{
-    title: 'Test Question 4',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: 'How do you find the minimum of x and y using JavaScript?',
+    choices: ['min(x,y)', 'Math.min(x,y)', 'Math.min(xy)', 'min(xy);'],
+    answer: 'Math.min(x,y)',
   },{
-    title: 'Test Question 5',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: 'Which built-in method returns the length of the string?',
+    choices: ['length()', 'size()', 'index()', 'None of the above'],
+    answer: 'length()',
   },{
-    title: 'Test Question 6',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: 'Which is the correct way to write a comment in JavaScript?',
+    choices: ['{# ... #}', '<!--- .... ---!>', '// ....', '\\ ...'],
+    answer: '// ....',
   },{
-    title: 'Test Question 7',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: 'How do you declare a new date in JavaScript?',
+    choices: ['var date = Date();', 'var date = date("now");', 'var date = new Date();', 'var date = date().current();'],
+    answer: 'var date = Date();',
   },
 ];
